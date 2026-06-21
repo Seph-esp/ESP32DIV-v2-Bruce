4a7d717cf0cf734d013cb66d8dce2ff68c550166
Seph-esp <nyauwusadist@gmail.com>
2026-06-21 22:35:21 +0000

    Add files via upload

--- boards/ESP32-DIV-v2/pins_arduino.h
+++ boards/ESP32-DIV-v2/pins_arduino.h
@@ -19,4 +19,13 @@ static const uint8_t SCK = 12;
 #define DEEPSLEEP_WAKEUP_PIN 0
 #define DEEPSLEEP_PIN_ACT LOW
 
+// NeoPixel LED configurations
+#define HAS_RGB_LED 1
+#define LED_COUNT 4
+#define RGB_LED 1
+#define LED_TYPE WS2812B
+#define LED_ORDER GRB
+#define LED_TYPE_IS_RGBW 0
+#define LED_COLOR_STEP 15
+
 #endif /* Pins_Arduino_h */
